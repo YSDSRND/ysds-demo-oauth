7f9822194218d74fab9480944d213e4db544d89d
Olle Strand <ollestrand02@gmail.com>
2022-05-11 13:34:16 +0200

    feat: demo complete

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import express, { Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import axios from 'axios';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 2999;
 
+// Client Details
+const OAUTH_CLIENT_ID = 'TEST_CLIENT'
+const OAUTH_SECRET = 'SecreT'
+const OAUTH_REDIRECT_URI = 'http://localhost:2999/cb'
+const OAUTH_SERVICE_URL = 'http://localhost:3004' //'https:/auth.cloud.ysds.com' 
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req: Request, res: Response) => {
+  res.render('index');
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
@@ -23,7 +33,23 @@ app.get('/cb', (req: Request, res: Response) => {
     return;
   }
 
+  axios.post(`${OAUTH_SERVICE_URL}/oauth/token?code=${code}&grant_type=code&redirect_uri=${OAUTH_REDIRECT_URI}&client_id=${OAUTH_CLIENT_ID}&client_secret=${OAUTH_SECRET}`).then(response => {
+    console.log(response.data);
+
+    //res.send(response.data);
+    res.render('signed-in', {
+      data: JSON.stringify(response.data, null, "\t")
+    });
+  }).catch(error => {
+    console.log(error.response);
+    
+    res.render('auth-failed');
+  });
+
+
   // Authenticate with code
+
+  
 });
 
 app.listen(port, () => {
